8983b28cfe4796d99771b39d3fc262cb187036e2
sreeramsreekumar200517 <sreeramsreekumar200517@gmail.com>
2026-05-25 15:12:05 +0530

    js change in jsfile

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('hi');
+let count = 10;
+let issreeram = true;
+let bignumber = 654556515555555n;
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(654556515555555n + 3n);
+let produtcts = null;
+let user;
+console.log(user);
+user ={
+    username:'sreeram'
+}
+let names =['manoj','sijo','gopi']
+console.log(name[2]);
+names[0] = 'joseph'
+console.log(name[0]);
